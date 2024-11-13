e204468466fcf2dc760a9fee919a7a4f90aead0e
SparkyPX <pierrelucproulx@gmail.com>
2024-11-13 12:29:28 -0500

    yes

--- script.js
+++ script.js
@@ -21,3 +21,24 @@ window.addEventListener('load', function() {
         mainContent.style.display = 'block';
     }, 500); // Wait for the fade-out to finish
 });
+
+
+$(function () {
+	var filterList = {
+		init: function () {
+			// MixItUp plugin
+			// http://mixitup.io
+			$('.portfolio-grid').mixItUp({
+				selectors: {
+  			  target: '.portfolio',
+  			  filter: '.filter'	
+  		  },
+  		  load: {
+    		  filter: 'all' // show app tab on first load
+    		}     
+			});								
+		}
+	};
+	// Run the show!
+	filterList.init();
+});	
